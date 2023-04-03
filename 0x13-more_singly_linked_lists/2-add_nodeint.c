ab90dcff4350e643ae7737645662122fde64ec6b
bennydreamtech23 <uwabunkeonyeijeoma@gmail.com>
2023-04-03 13:43:57 -0700

    updates

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -9,12 +9,12 @@
 
 listint_t *add_nodeint(listint_t **head, const int n)
 {
-	 listint_t *countfull;
-
-	 countfull = malloc(sizeof(listint_t))
-		 if (countfull == NULL)
-			 return (NULL)
-				 countfull->n = n
-				 countfull->next = *head
-				 return (head);
+	listint_t *countfull;
+	
+	countfull = malloc(sizeof(listint_t))
+		if (countfull == NULL)
+			return (NULL)
+				countfull->n = n
+				countfull->next = *head
+				return (head);
 }
