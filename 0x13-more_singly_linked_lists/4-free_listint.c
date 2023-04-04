3d53428b10e33ce08ccc6e2a21074b0ba126afd1
bennydreamtech23 <uwabunkeonyeijeoma@gmail.com>
2023-04-04 02:41:13 -0700

    c

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * free_listint - frees listint_t list.
+ * @head: Address of the first list.
+ **/
+
+void free_listint(listint_t *head)
+{
+
+	listint_t *ch, *ch2;
+
+	ch = head;
+
+	while (ch != NULL)
+	{
+	
+		ch2 = ch->next;
+		free(ch);
+		ch = ch2;
+	}
+}
