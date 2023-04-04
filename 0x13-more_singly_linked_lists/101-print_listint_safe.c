6d981f35975311becd3fcf3a3f6d0e3cca00c6d3
UWABUNKEONYE BENEDICTA <101554359+bennydreamtech23@users.noreply.github.com>
2023-04-04 16:30:33 +0100

    Update 101-print_listint_safe.c

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -5,8 +5,7 @@
  * @head: list of type listint_t to print.
  * Return: number of nodes in the list
  **/
-
-size_t print_listint_safe(const listint_t *head)
+size_t print_listint_safe(const listint_t *head);
 {
 	size_t num = 0;
 	long int diff;
